9c7c02604cd3803aa43bd02f69448aad2d45ca48
Soo Yeon Shim <sooyshim@gmail.com>
2019-06-02 22:56:47 -0400

    Updates

---
+++ precache-manifest.e5a944a06694e754c77bf75dfa979a1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313eb3fdd313c3bd203d380770d47410",
+    "url": "/ahronspeckTheSpeakingStatue/index.html"
+  },
+  {
+    "revision": "8118bc2f4b2d1064a4fd",
+    "url": "/ahronspeckTheSpeakingStatue/static/css/main.18003496.chunk.css"
+  },
+  {
+    "revision": "c6b8dde340bb6a7609c3",
+    "url": "/ahronspeckTheSpeakingStatue/static/js/2.6561b881.chunk.js"
+  },
+  {
+    "revision": "8118bc2f4b2d1064a4fd",
+    "url": "/ahronspeckTheSpeakingStatue/static/js/main.b0f48fa0.chunk.js"
+  },
+  {
+    "revision": "1d69b71d973726026d1e",
+    "url": "/ahronspeckTheSpeakingStatue/static/js/runtime~main.ab8e9e75.js"
+  },
+  {
+    "revision": "9db7a8333b63df08859ae38a66ac1181",
+    "url": "/ahronspeckTheSpeakingStatue/static/media/large.9db7a833.png"
+  },
+  {
+    "revision": "6141336f49bf9cf722b00dbaa509b827",
+    "url": "/ahronspeckTheSpeakingStatue/static/media/parchemin.6141336f.png"
+  },
+  {
+    "revision": "4c861d0241861dc248b6cca3792cf4c3",
+    "url": "/ahronspeckTheSpeakingStatue/static/media/small.4c861d02.png"
+  }
+]);
